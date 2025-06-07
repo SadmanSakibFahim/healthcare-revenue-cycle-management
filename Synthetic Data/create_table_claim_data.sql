6e5676bd68e845d37cee8ae338b28f17a2f00e0b
Sadman Sakib Fahim <64649472+SadmanSakibFahim@users.noreply.github.com>
2025-06-08 00:50:12 +0600

    Update create_table_claim_data.sql

--- Synthetic Data/create_table_claim_data.sql
+++ Synthetic Data/create_table_claim_data.sql
@@ -1,18 +1,18 @@
 CREATE TABLE claim_data(
 	Claim_ID VARCHAR(40),
-    Provider_ID DOUBLE,
+	Provider_ID DOUBLE,
 	Patient_ID VARCHAR(40),	
-    Date_of_Service VARCHAR(40),	
-    Billed_Amount DOUBLE,
-    Procedure_Code INT(32),
-    Diagnosis_Code VARCHAR(40),
+	Date_of_Service VARCHAR(40),	
+	Billed_Amount DOUBLE,
+	Procedure_Code INT(32),
+	Diagnosis_Code VARCHAR(40),
 	Allowed_Amount DOUBLE,
 	Paid_Amount DOUBLE, 
 	Insurance_Type VARCHAR(40),
 	Claim_Status VARCHAR(40),	
-    Reason_Code VARCHAR(80), 
-    Follow_up_Required VARCHAR(40), 
-    AR_Status VARCHAR(40),	
-    Outcome VARCHAR(40)
-    )
+	Reason_Code VARCHAR(80), 
+	Follow_up_Required VARCHAR(40), 
+	AR_Status VARCHAR(40),	
+	Outcome VARCHAR(40)
+	)
     
